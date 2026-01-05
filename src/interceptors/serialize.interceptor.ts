5f8ef90c9d8242ecac1df5a6d2bb53f8a4d022df
Mikhael Candra <tjandraraharja99@gmail.com>
2026-01-05 15:41:39 +0700

    how to implement expose

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,12 +1,14 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
 import { map, Observable } from 'rxjs';
+import { UserDto } from 'src/users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     // Run something before a request is handled by the request handler
     return next.handle().pipe(
       map((data: any) => {
-        console.log('Intercepted Response', data);
+        return plainToClass(UserDto, data, { excludeExtraneousValues: true });
       }),
     );
   }
