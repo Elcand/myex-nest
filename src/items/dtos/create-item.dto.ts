3dddabf5207ff31dd114f17ea04214be1a2d06c2
Mikhael Candra <tjandraraharja99@gmail.com>
2026-01-07 16:34:33 +0700

    set validator on item dto

--- src/items/dtos/create-item.dto.ts
+++ src/items/dtos/create-item.dto.ts
@@ -1,7 +1,15 @@
+import { isNumber, isString, Min } from "class-validator";
+
 export class CreateItemDto {
+  @isString()
   name: string;
+  @isString()
   description: string;
+  @isNumber()
+  @Min(0)
   price: number;
+  @isString()
   location: string;
+  @isString() 
   category: string;
 }
