51314fd1a8784eb7d276cd00370964a49ca80271
Mikhael Candra <tjandraraharja99@gmail.com>
2026-01-07 09:43:40 +0700

    initial setup of unit test files

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersService } from 'src/users/users.service';
+import { UsersService } from '../users/users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/users/users.entity';
+import { User } from '../users/users.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
+    const fakeUserService = {
+      find: () => Promise.resolve([]),
+      create: () =>
+        Promise.resolve({
+          id: 1,
+          name: 'John Doe',
+          email: 'jhon@doe',
+          password: '1234567890',
+        }),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: UsersService, useValue: fakeUserService },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
