535996440ae821c95797518da7368300ec7b1e28
Mikhael Candra <tjandraraharja99@gmail.com>
2026-01-19 09:37:08 +0700

    change  response create item with transform class

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -4,12 +4,15 @@ import { ItemsService } from './items.service';
 import { AuthGuard } from '../guards/auth.guard';
 import { CurrentUser } from '../auth/decorators/current-user.decorator';
 import { User } from '../users/users.entity';
+import { ItemDto } from './dtos/item.dto';
+import { Serialize } from '../interceptors/serialize.interceptor';
 
 @Controller('items')
 export class ItemsController {
   constructor(private ItemService: ItemsService) {}
   @Post()
   @UseGuards(AuthGuard)
+  @Serialize(ItemDto)
   createItem(@Body() body: CreateItemDto, @CurrentUser() user: User) {
     return this.ItemService.create(body, user);
   }
